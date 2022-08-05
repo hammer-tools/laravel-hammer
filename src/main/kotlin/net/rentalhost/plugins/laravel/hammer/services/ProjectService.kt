af3988f08c17e5201869f352b78038c1dce359b1
David Rodrigues <david.rodrigues@rentalhost.net>
2022-08-05 19:26:33 -0300

    [Changed] Plugin: update notificationGroup to be project exclusive;

--- src/main/kotlin/net/rentalhost/plugins/laravel/hammer/services/ProjectService.kt
+++ src/main/kotlin/net/rentalhost/plugins/laravel/hammer/services/ProjectService.kt
@@ -10,6 +10,8 @@ class ProjectService: BaseProjectService() {
 
     override val name: String = "Laravel Hammer"
 
+    override val notificationGroup: String = "net.rentalhost.plugins.laravel.hammer.notifications"
+
     override val sentryDsn: String = "https://8c46d0ca554447f8827b58c46e36a923@o55698.ingest.sentry.io/6627285"
 
     override val urls: UrlService = object: UrlService() {
